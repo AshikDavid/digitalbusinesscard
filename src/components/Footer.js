7381e4a18f1828f031c32c45797297141058ad88
ashik david <ashikdavid007@gmail.com>
2024-08-07 19:01:14 +0530

    Resolved error in the ESLint configuration.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ import Git from "../images/GitHub-icon.png"
 export default function Footer(){
     return(
         <div className="footer">
-        <button className="footer-icon"><img src={Twitter}  /></button>
-        <button className="footer-icon"><img src={FB} /></button>
-        <button className="footer-icon"><img src={Insta} /></button>
-        <button className="footer-icon"><img src={Git} /></button>
+        <button className="footer-icon"><img src={Twitter} alt="twitter" /></button>
+        <button className="footer-icon"><img src={FB} alt="Facebook"/></button>
+        <button className="footer-icon"><img src={Insta} alt="Instagram"/></button>
+        <button className="footer-icon"><img src={Git} alt="Github"/></button>
         
         </div>
     )
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,19 +2,19 @@ import React from "react"
 import "./Info.css";
 import Profile from "../images/Profile-pic.png"
 import Email from "../images/email-icon.png";
-import LinkeDin from "../images/linkedin-icon.png"
+import Linkedin from "../images/linkedin-icon.png"
 
 export default function Info(){
     return(
         <div className="info">
-            <img className="info-pic" src={Profile} />
+            <img className="info-pic" src={Profile} alt="Profile Picture" />
             <div>
                 <h2 className="info--name">Laura Smith</h2>
                 <h3 className="info--designation">Frontend Developer</h3>
                 <a href="" className="info--website">laurasmith.website</a>
                 <div className="info--buttons">
-                 <button className="info--button--email"><img src={Email}/>Email</button>
-                 <button className="info--button--linkedin"><img src ={LinkeDin}/> LinkedIn</button>
+                 <button className="info--button--email"><img src={Email} alt="Email"/>Email</button>
+                 <button className="info--button--linkedin"><img src ={Linkedin} alt="Linkedin"/> LinkedIn</button>
                 
              </div>
             </div>
